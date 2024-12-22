fdfb40c6177f0b5caf5bebfb0e29e8bcb70f6f08
Krish Chandra <krishuchandra1783@gmail.com>
2024-12-22 22:19:00 +0530

    Delete crop_prediction directory

--- crop_prediction/predictor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('predict-crop/', views.predict_crop, name='predict_crop'),
-]
